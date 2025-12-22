488dd2044fba3e62d8284ff7f449ee5aba53fa98
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2025-12-22 14:48:28 +0000

    fix: don't class 404/409 as span errors on the HTTP client

--- libraries/common/pkg/tracefx/http_client.go
+++ libraries/common/pkg/tracefx/http_client.go
@@ -4,8 +4,37 @@ import (
 	"net/http"
 
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
 )
 
 // DefaultHTTPClient is a tracing-enabled HTTP client for internal uses.
 // It adopts trace headers into the context for requests, and forwards it on via headers for any requests made.
-var DefaultHTTPClient = &http.Client{Transport: otelhttp.NewTransport(http.DefaultTransport)}
+var DefaultHTTPClient = &http.Client{
+	Transport: &clientTransport{
+		rt: otelhttp.NewTransport(http.DefaultTransport),
+		okStatusCodes: map[int]bool{
+			http.StatusNotFound: true,
+			http.StatusConflict: true,
+		},
+	},
+}
+
+type clientTransport struct {
+	rt http.RoundTripper
+
+	// okStatusCodes are codes that aren't usually considered successful (4xx/5xx), but should be.
+	// Not included status codes are ignored and use whatever OTEL set as the status code.
+	okStatusCodes map[int]bool
+}
+
+func (t *clientTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	resp, err := t.rt.RoundTrip(req)
+
+	if err == nil && t.okStatusCodes[resp.StatusCode] {
+		span := trace.SpanFromContext(req.Context())
+		span.SetStatus(codes.Ok, "")
+	}
+
+	return resp, err
+}
