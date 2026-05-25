b307ed891bf01fb4bb8bcfed0155bf40b085dd0f
mworzala <mattheworzala@gmail.com>
2026-05-25 15:10:55 -0400

    chore: more slop logs

--- api/v1Public/server_map.go
+++ api/v1Public/server_map.go
@@ -7,6 +7,7 @@ import (
 	"github.com/hollow-cube/api-server/api/auth"
 	"github.com/hollow-cube/api-server/internal/mapdb"
 	"github.com/hollow-cube/api-server/pkg/ox"
+	"go.uber.org/zap"
 )
 
 type MapRequest struct {
@@ -15,11 +16,13 @@ type MapRequest struct {
 
 func (s *Server) mapForAuthPlayer(ctx context.Context, mapID string) (*mapdb.Map, error) {
 	playerID, ok := auth.GetPlayerID(ctx)
+	zap.S().Infow("mapForAuthPlayer", "player id", playerID, "ok", ok, "map id", mapID)
 	if !ok {
 		return nil, ox.Unauthorized{}
 	}
 
 	m, err := s.mapStore.GetMapById(ctx, mapID)
+	zap.S().Infow("mapForAuthPlayer: got map", "map", m, "err", err)
 	if errors.Is(err, mapdb.ErrNoRows) {
 		return nil, ox.NotFound{}
 	}
